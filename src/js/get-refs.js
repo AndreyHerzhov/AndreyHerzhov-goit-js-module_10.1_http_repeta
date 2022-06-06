5d5fddf81cd76adfad60697646fad28ff7be885a
AndreyHerzhov <andreyherzhov@gmail.com>
2022-06-06 11:52:51 +0300

    delete

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -1,6 +1,1 @@
-export default function getRefs() {
-  return {
-    cardContainer: document.querySelector('.js-card-container'),
-    searchForm: document.querySelector('.js-search-form'),
-  };
-}
+ 
